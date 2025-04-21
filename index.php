fa01c01f17fcf83205d3e798dc073051d5b6b86c
Pedro A <pether.1994@gmail.com>
2025-04-21 16:30:49 -0600

    feat(index):  vista principal con listado de tareas

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<?php
+require 'db.php';
+
+$stmt = $pdo->query("SELECT * FROM tasks ORDER BY created_at DESC");
+$tasks = $stmt->fetchAll();
+?>
 
 <!DOCTYPE html>
 <html lang="es">
@@ -11,8 +17,23 @@
 <body>
     <h1>Lista de Tareas</h1>
 
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <form id="task-form">
+        <input type="text" name="task_name" id="task_name" placeholder="Nueva tarea" required>
+        <button type="submit">Añadir</button>
+    </form>
 
+    <ul id="task-list">
+        <?php foreach ($tasks as $task): ?>
+            <li>
+                <?= htmlspecialchars($task['task_name']) ?>
+                [<a href="edit.php?id=<?= $task['id'] ?>">Editar</a>]
+                [<a href="#" class="delete-task" data-id="<?= $task['id'] ?>">Eliminar</a>]
+            </li>
+        <?php endforeach; ?>
+    </ul>
+
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+    <script src="js/main.js"></script>
 </body>
 
 </html>
